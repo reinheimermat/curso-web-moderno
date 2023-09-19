770e01ded62a16712446d6542ecb22740f6d9316
reinheimermat <mreinheimer0@gmail.com>
2023-09-19 10:47:27 -0300

    commit

--- exercicios-js/esnext/asyncAwait.js
+++ exercicios-js/esnext/asyncAwait.js
@@ -20,3 +20,16 @@ const getTurma = (letra) => {
     })
   })
 }
+
+// Recurso de ES8
+// Objetivo de simplificar o uso de promises...
+let obterAlunos = async () => {
+    const ta = await getTurma('A')
+    const tb = await getTurma('B')
+    const tc = await getTurma('C')
+    return [].concat(ta, tb, tc)
+} // retorna um objeto AsyncFunction
+
+obterAlunos()
+    .then(alunos => alunos.map(a => a.nome))
+    .then(nomes => console.log(nomes))
