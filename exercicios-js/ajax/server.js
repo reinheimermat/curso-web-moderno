cce4ebfeb4bf4aa1539178de8684767d5ea031a0
reinheimermat <mreinheimer0@gmail.com>
2023-09-27 11:47:50 -0300

    update server

--- exercicios-js/ajax/server.js
+++ exercicios-js/ajax/server.js
@@ -40,7 +40,10 @@ app.get('/parOuImpar', (req, res) => {
     // req.body
     // req.query
     // req.params
-    const par = parseInt()
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
 })
 
 app.listen(8080, () => console.log('Executando...'))
