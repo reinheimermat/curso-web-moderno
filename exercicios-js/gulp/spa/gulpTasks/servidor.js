12671933c63dc29caa5c51e2d9f67167ff09710e
reinheimermat <mreinheimer0@gmail.com>
2023-09-28 16:53:41 -0300

    get started jquery

--- exercicios-js/gulp/spa/gulpTasks/servidor.js
+++ exercicios-js/gulp/spa/gulpTasks/servidor.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
 function monitorarArquivos(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')())
     return cb()
 }
 
-function servidor(cb) {
-    return cb()
+function servidor() {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080,
+            open: true,
+            livereload: true,
+        }))
 }
 
 module.exports = {
---
+++ exercicios-js/webpack/src/pessoa.js
@@ -0,0 +1,7 @@
+import './modules/moduloA'
+
+export default class Pessoa {
+    cumprimentar() {
+        return 'Bom dia!'
+    }
+}
